319b5853cce1d9bee4c1388a54b7b228e8e9798c
Hemant Jadhav <webdev.hemant@example.com>
2023-04-13 11:26:55 +0530

    nav routes complete

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,18 +1,26 @@
-import { useState } from "react";
-import { Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import { RxCross2 } from "react-icons/rx";
 import { GiHamburgerMenu } from "react-icons/gi";
 import OutsideClickHandler from "../OutsideClickHandler";
+import { Routes } from "../../constants/Routes";
 import styles from "./sidebar.module.scss";
 
 const Sidebar = () => {
   const [showNav, setShowNav] = useState(true);
-  // const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const [activePath, setactivePath] = useState(location.pathname);
+  console.log(activePath);
+  useEffect(() => {
+    setactivePath(location.pathname);
+  }, [location.pathname]);
 
   return (
     <>
       <nav className={styles.navWrapper}>
-        <div className={`${styles.mobileNav} `}>
+        <div className={`${styles.navContainer} `}>
           <div className={styles.imgWrapper}></div>
           {showNav ? (
             <GiHamburgerMenu
@@ -23,6 +31,22 @@ const Sidebar = () => {
               className={styles.hamBurger}
             />
           ) : null}
+          <ul className={styles.routesWrapper}>
+            {Routes.map((item) => {
+              console.log(activePath === item.path);
+              return (
+                <li
+                  onClick={() => navigate(item.path)}
+                  key={item}
+                  className={`${
+                    activePath === item.path ? styles.activeRoute : ""
+                  }`}
+                >
+                  {item.name}
+                </li>
+              );
+            })}
+          </ul>
         </div>
         <OutsideClickHandler onOutsideClick={() => setShowNav(true)}>
           <div
@@ -38,19 +62,29 @@ const Sidebar = () => {
                 style={{ width: "40px", height: "40px", stroke: "white" }}
                 className={styles.crossIcon}
               />
-              {/* {sidebarState
-                .filter((item) => item.path !== "*")
-                .map((item, index) => (
-                  <li key={index} className={styles.titleName}>
-                    <CollapsableComponent routeDetail={item} />
+              {Routes.map((item, index) => {
+                return (
+                  <li
+                    key={index}
+                    onClick={() => navigate(item.path)}
+                    className={styles.titleWrapper}
+                  >
+                    <h4
+                      className={`${styles.title} ${
+                        activePath === item.path ? styles.acitveRoute : ""
+                      }`}
+                    >
+                      {item.name}
+                    </h4>
+                    {/* <CollapsableComponent routeDetail={item} />/ */}
                   </li>
-                ))} */}
+                );
+              })}
             </ul>
           </div>
         </OutsideClickHandler>
       </nav>
       <main>
-        {/* {location.pathname !== "/" ? <Breadcrumbs /> : null} */}
         <Outlet />
       </main>
     </>
---
+++ src/constants/Routes.js
@@ -0,0 +1,13 @@
+const Routes = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Dashboard",
+    path: "/dashboard",
+  },
+];
+Object.freeze(Routes);
+
+export { Routes };
